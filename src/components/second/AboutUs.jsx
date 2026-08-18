ec5089d0f52ce51b7794b8f377209a1ff6aa8139
kandevishnu <kandevishnu1234@gmail.com>
2026-08-18 22:41:14 +0530

    section 2 responsive

--- src/components/second/AboutUs.jsx
+++ src/components/second/AboutUs.jsx
@@ -3,27 +3,47 @@ import img1 from "./1.png";
 import img2 from "./2.png";
 const AboutUs = () => {
   return (
-    <div className="h-screen flex items-center justify-around p-20 font-[Space_Grotesk]">
-      <div className="w-[35%]">
-        <h1 className="font-medium text-7xl py-5">Stacking, a new way to earn bitcoin</h1>
-        <p className="py-5">
+    <div className="h-screen flex items-center justify-around p-[5%] font-[Space_Grotesk]">
+      <div className="sm:w-[35%] w-[70%]">
+        <h1 className="font-medium lg:text-7xl sm:py-[4%] sm:text-5xl md:text-6xl text-5xl">
+          Stacking, a new way to earn bitcoin
+        </h1>
+        <p className="py-[4%] sm:max-md:leading-5 ">
           Hold and temporarily lock STX, stacks native currency, and support the
           networks security and consensus. As a rewards, you`ll earn BTC.
         </p>
-        <button className="bg-[#5846FB] text-white px-5 py-2 rounded-full">Start Stacking</button>
+        <div className="text-end sm:text-left">
+          <button className="bg-[#5846FB] text-white px-5 py-[4%] rounded-full">
+            Start Stacking
+          </button>
+        </div>
       </div>
-      <div className="w-[45%]">
+      <div className="w-[45%] hidden sm:block">
         <div className="flex items-center gap-4 mb-3">
-          <p className="bg-[#f0f0f0] text-5xl rounded-full px-10 py-5">9.5%</p>
-          <img src={img1} alt="image" className="h-[5em]" />
+          <p className="bg-[#f0f0f0] lg:text-5xl rounded-full px-10 py-5 md:max-lg:text-3xl sm:text-2xl">
+            9.5%
+          </p>
+          <img
+            src={img1}
+            alt="image"
+            className="lg:h-[5em] sm:max-lg:h-[4em] sm:max-lg:w-[4em] h-[3em]"
+          />
         </div>
         <div className="flex justify-between bg-[#5846FB] text-white px-7 py-5 mb-3 rounded-full items-center">
-          <p className="text-5xl">$864,896,126</p>
-          <p>Total value locked</p>
+          <p className="lg:text-5xl md:max-lg:text-3xl sm:text-2xl">
+            $864,896,126
+          </p>
+          <p className="text-end sm:max-lg:text-sm">Total value locked</p>
         </div>
         <div className="flex items-center justify-end gap-4">
-          <img src={img2} alt="" className="h-[5em]" />
-          <p className="font-semibold text-2xl w-70 rounded-full bg-[#f0f0f0] px-10 py-5">Data courtesy of stacking club</p>
+          <img
+            src={img2}
+            alt=""
+            className="lg:h-[5em] sm:max-lg:h-[4em] sm:max-lg:w-[4em] h-[3em]"
+          />
+          <p className="font-semibold lg:text-2xl w-70 rounded-full bg-[#f0f0f0] px-[10%] lg:px-10 py-5 sm:max-lg:text-sm">
+            Data courtesy of stacking club
+          </p>
         </div>
       </div>
     </div>
